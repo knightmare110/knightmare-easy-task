314c465b1b1770677210667bf9c820b46e634e63
knightmare110 <knigtmare.protagonist@gmail.com>
2024-06-27 03:38:43 -0400

    Finished task listing and complete feature

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-  computed,
-  input,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { type User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -15,20 +9,15 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-
-  // imagePath = computed(() => 'assets/users/' + this.avatar());
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }

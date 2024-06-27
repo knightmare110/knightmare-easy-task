d5e666c4f316f53795a8eeb240695b29b9881792
knightmare110 <knigtmare.protagonist@gmail.com>
2024-06-27 05:45:46 -0400

    complete task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,48 +1,36 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTask } from './task/task.model';
+import { TasksService } from './tasks.service';
 @Component({
-    selector: 'app-tasks',
-    standalone: true,
-    templateUrl: './tasks.component.html',
-    styleUrl: './tasks.component.css',
-    imports: [TaskComponent]
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
-  @Input({required: true}) name?: string;
-  @Input({required: true}) userId!: string;
+  @Input({ required: true }) name?: string;
+  @Input({ required: true }) userId!: string;
+  isShowAddModal: boolean = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+  onStartAddTask() {
+    this.isShowAddModal = true;
+  }
+
+  onCloseAddTask() {
+    this.isShowAddModal = false;
+  }
+
+  onAddTask(taskData: NewTask) {
+    this.isShowAddModal = false;
+    this.tasksService.addTask(taskData, this.userId);
   }
 }
